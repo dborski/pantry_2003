b47c6c8bbad221d87cfe536ec6c82d9eecfd1667
Derek Borski <derek.borski@gmail.com>
2020-04-22 09:27:42 -0600

    ingredients_required and add_ingredients methods

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -1,18 +1,33 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/ingredient'
-require './lib/pantry'
+# require './lib/pantry'
 require './lib/recipe'
 
 class RecipeTest < Minitest::Test
 
   def setup
     @recipe1 = Recipe.new("Mac and Cheese")
+    @ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+    @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
   end
 
   def test_it_exists
-    assert_instance_of Recipe, @recipe
+    assert_instance_of Recipe, @recipe1
   end
 
+  def test_it_has_readable_attributes
+    assert_equal "Mac and Cheese", @recipe1.name
+  end
 
+  def test_can_add_required_ingredients
+    assert_equal ({}), @recipe1.ingredients_required
+
+    @recipe1.add_ingredient(@ingredient1, 2)
+    @recipe1.add_ingredient(@ingredient1, 4)
+    @recipe1.add_ingredient(@ingredient2, 8)
+
+    expected = {@ingredient1 => 6, @ingredient2 => 8}
+    assert_equal expected, @recipe1.ingredients_required
+  end
 end
