769e86c597b8d4b827af77fff534d552064f8704
Derek Borski <derek.borski@gmail.com>
2020-04-22 09:05:12 -0600

    Attributes

--- test/ingredient_test.rb
+++ test/ingredient_test.rb
@@ -9,6 +9,12 @@ class IngredientTest < Minitest::Test
   end
 
   def test_it_exists
-    assert_instance_of Ingredient, @ingredient
+    assert_instance_of Ingredient, @ingredient1
+  end
+
+  def test_it_has_readable_attributes
+    assert_equal "Cheese", @ingredient1.name
+    assert_equal "oz", @ingredient1.unit
+    assert_equal 50, @ingredient1.calories
   end
 end
