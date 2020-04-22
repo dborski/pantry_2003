e41409e513bdb2ad02da086370cb097129bfdf05
Derek Borski <derek.borski@gmail.com>
2020-04-22 09:16:07 -0600

    stock and restock methods

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,3 +1,11 @@
 class Pantry
 
+  attr_reader :stock
+  def initialize
+    @stock = Hash.new(0)
+  end
+
+  def restock(ingredient, quantity)
+    @stock[ingredient] += quantity
+  end
 end
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -7,9 +7,26 @@ class PantryTest < Minitest::Test
 
   def setup
     @pantry = Pantry.new
+
+    @ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+    @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
   end
 
   def test_it_exists
     assert_instance_of Pantry, @pantry
+  end
+
+  def test_can_stock_ingredients
+    assert_equal ({}), @pantry.stock
+
+    @pantry.restock(@ingredient1, 5)
+
+    expected = {@ingredient1 => 5}
+    assert_equal expected, @pantry.stock
+
+    @pantry.restock(@ingredient1, 10)
+
+    expected = {@ingredient1 => 15}
+    assert_equal expected, @pantry.stock
   end
 end
