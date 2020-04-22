a5368da0aee29eb102d0c17414bcd0190d26b399
Derek Borski <derek.borski@gmail.com>
2020-04-22 09:56:03 -0600

    ingredients method

--- lib/cook_book.rb
+++ lib/cook_book.rb
@@ -8,4 +8,12 @@ class CookBook
   def add_recipe(recipe)
     @recipes << recipe
   end
+
+  def ingredients
+    @recipes.map do |recipe|
+      recipe.ingredients.map do |ingredient, quantity|
+        ingredient.name
+      end
+    end.flatten.uniq
+  end
 end
